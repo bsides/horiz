d5af72a0234783c4bddf64a302714af53f1146d7
Rafael Pereira <rafaelbsides at gmail.com>
2024-04-22 16:20:01 -0300

    Adds some styles

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
 import { useCombatData } from './context/useCombatData'
 
 export function Overlay() {
   const data = useCombatData()
 
+  if (data) {
+    return (
+      <div className="overlay">
+        {data?.combatants.map((combatant) => (
+          <div key={combatant.name + combatant.deaths + combatant.DPS}>
+            <div>{combatant.name}</div>
+            <div>DPS: {combatant.ENCDPS}</div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <div>
-      {data?.combatants.map((combatant) => (
-        <React.Fragment key={combatant.name + combatant.deaths + combatant.DPS}>
-          <div>{combatant.name}</div>
-          <div>DPS: {combatant.ENCDPS}</div>
-        </React.Fragment>
-      ))}
+    <div className="nodata">
+      <header>
+        <p>H O R I Z</p>
+        <p>Awaiting for combat data...</p>
+      </header>
     </div>
   )
 }
